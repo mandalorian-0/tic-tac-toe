414396ff2d3e92d9eccb770cd70303317407bf02
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-03-19 07:28:37 -0700

    add project ui structure and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
 import { createGameEngine } from "./engine.js";
 
-// const game = createGameEngine();
-
-// game.startGame();
-
-// game.makeMove(0);
-// game.makeMove(4);
-// game.makeMove(1);
-// game.makeMove(3);
-// game.makeMove(2);
-
-// console.log(game.getBoard());
-// console.log(game.isGameOver());
+const game = createGameEngine();
